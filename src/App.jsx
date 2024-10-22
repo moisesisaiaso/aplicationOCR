8470018ced28538e3f73b32e0ff560afe78ba2dc
Moisés Isaías Ortíz Gracia <moisesisaias23mar@gmail.com>
2024-10-22 16:37:17 -0500

    aplicationOCR-02

--- src/App.jsx
+++ src/App.jsx
@@ -26,45 +26,47 @@ function App() {
 
     return (
         <>
-            <header>
-                <Header />
-            </header>
-            <main className={generalStyles.container_frame}>
-                {/* inputFile */}
-                <section className={generalStyles.container_inputFile}>
-                    <InputFile
-                        uploadImage={uploadImage}
-                        setUploadImage={setUploadImage}
-                        setExpand={setExpand}
-                        setUploadFile={setUploadFile}
-                    />
-                    {/* modal expand */}
-                    <ModalExpand
-                        expand={expand}
-                        setExpand={setExpand}
-                        uploadImage={uploadImage}
-                        uploadFile={uploadFile}
-                    />
-                </section>
+            <div className={generalStyles.containerGrid}>
+                <header>
+                    <Header />
+                </header>
+                <main className={generalStyles.container_frame}>
+                    <div className={generalStyles.containerGroup}>
+                        {/* inputFile */}
+                        <section className={generalStyles.container_inputFile}>
+                            <InputFile
+                                uploadImage={uploadImage}
+                                setUploadImage={setUploadImage}
+                                setExpand={setExpand}
+                                setUploadFile={setUploadFile}
+                            />
+                            {/* modal expand */}
+                            <ModalExpand
+                                expand={expand}
+                                setExpand={setExpand}
+                                uploadImage={uploadImage}
+                                uploadFile={uploadFile}
+                            />
+                        </section>
 
-                {/* lenguajes */}
-                <section className={generalStyles.container_languages}>
-                    <Languages
-                        selectedLanguages={selectedLanguages}
-                        setSelectedLanguages={setSelectedLanguages}
-                    />
-                </section>
+                        {/* lenguajes */}
+                        <section className={generalStyles.container_languages}>
+                            <Languages
+                                selectedLanguages={selectedLanguages}
+                                setSelectedLanguages={setSelectedLanguages}
+                            />
+                        </section>
 
-                {/* Extraer texto */}
-                <section className={generalStyles.container_btnGetText}>
-                    <ButtonGetText completeData={completeData} />
-                </section>
-
-                {/* GetTextOrc */}
-                <section className={generalStyles.containerGetTextOcr}>
-                    <GetTextOcr uploadFile={uploadFile} selectedLanguages={selectedLanguages} />
-                </section>
-
+                        {/* Extraer texto */}
+                        <section className={generalStyles.container_btnGetText}>
+                            <ButtonGetText completeData={completeData} />
+                        </section>
+                    </div>
+                    {/* GetTextOrc */}
+                    <section className={generalStyles.containerGetTextOcr}>
+                        <GetTextOcr uploadFile={uploadFile} selectedLanguages={selectedLanguages} />
+                    </section>
+                </main>
                 {/* mascara de fondo */}
                 <div
                     className={
@@ -76,14 +78,14 @@ function App() {
                         setExpand(false);
                     }}
                 ></div>
-            </main>
-            <footer>
-                <span></span>
-                <h5>&copy; DESARROLLADO POR MOISES ISAIAS ORTIZ GRACIA</h5>
-                <span>
-                    <img src="" alt="" />
-                </span>
-            </footer>
+                <footer>
+                    <span></span>
+                    <h5>&copy; DESARROLLADO POR MOISES ISAIAS ORTIZ GRACIA</h5>
+                    <span>
+                        <img src="" alt="" />
+                    </span>
+                </footer>
+            </div>
         </>
     );
 }
